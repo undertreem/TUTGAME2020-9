3ddd1352fb2fca84e450d599c0e0e7540acb6796
LAPTOP-O7AM0704\m0119320 <m011932016@edu.teu.ac.jp>
2020-07-21 12:01:34 +0900

    no message

---
+++ Assets/Scenes/SYOUGEKIHA/Sphere.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public GameObject prefab;
+    private bool flag = true;
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && flag == true)
+        {
+            StartCoroutine(CreateWave());
+            flag = false;
+        }
+    }
+    IEnumerator CreateWave()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Instantiate(prefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(1);
+        }
+        flag = true;
+        yield break;
+    }
+}
