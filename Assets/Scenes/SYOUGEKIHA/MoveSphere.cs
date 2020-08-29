c396cf5b2e1b3bcf1f79baa45578ca02cae493cc
LAPTOP-O7AM0704\m0119320 <m011932016@edu.teu.ac.jp>
2020-08-29 21:41:15 +0900

    no message

--- Assets/Scenes/SYOUGEKIHA/MoveSphere.cs
+++ Assets/Scenes/SYOUGEKIHA/MoveSphere.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 public class MoveSphere : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject player;
     void Start()
     {
         Application.targetFrameRate = 60;
         Invoke("BrokeWave", 0.8f);
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     void Update()
     {
         transform.localScale += new Vector3(0.4f, 0.4f, 0.4f);
-
+        this.gameObject.transform.position = player.transform.position;
     }
     void BrokeWave()
     {
