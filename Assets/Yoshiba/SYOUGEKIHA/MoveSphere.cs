c010ebf0c4171ad49cbd4a7bb36cc1583b358ba0
Kinoshita Ryoki <m01191002e@edu.teu.ac.jp>
2020-08-30 19:06:11 +0900

    MoveSphere.cs反映

--- Assets/Yoshiba/SYOUGEKIHA/MoveSphere.cs
+++ Assets/Yoshiba/SYOUGEKIHA/MoveSphere.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+​
 public class MoveSphere : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject player;
     void Start()
     {
         Application.targetFrameRate = 60;
         Invoke("BrokeWave", 0.8f);
+        player = GameObject.FindGameObjectWithTag("Player");
     }
     void Update()
     {
         transform.localScale += new Vector3(0.4f, 0.4f, 0.4f);
-
+        this.gameObject.transform.position = player.transform.position;
     }
     void BrokeWave()
     {
