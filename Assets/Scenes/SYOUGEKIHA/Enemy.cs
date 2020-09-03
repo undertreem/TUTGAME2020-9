b706f2b092f12d56f1492e11c96bac8e7aee2886
LAPTOP-O7AM0704\m0119320 <m011932016@edu.teu.ac.jp>
2020-09-04 02:54:18 +0900

    no message

--- Assets/Scenes/SYOUGEKIHA/Enemy.cs
+++ Assets/Scenes/SYOUGEKIHA/Enemy.cs
@@ -9,17 +9,27 @@ public class Enemy : MonoBehaviour
     private GameObject player;
     private NavMeshAgent nav;
     private Vector3 targetPos;
+    private float eneDis;//追加
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         nav = this.gameObject.GetComponent<NavMeshAgent>();
+        eneDis = 7.0f;//追加
     }
 
     // Update is called once per frame
     void Update()
     {
         targetPos = player.transform.position;
-        nav.destination = targetPos;
+        //この下変更＆追加
+        if (Vector3.Distance(targetPos, this.transform.position) <= eneDis)
+        {
+            nav.destination = targetPos;
+        }
+        else
+        {
+            nav.destination = this.transform.position;
+        }
     }
 }
--- Assets/Scenes/SYOUGEKIHA/Sphere.cs
+++ Assets/Scenes/SYOUGEKIHA/Sphere.cs
@@ -6,16 +6,25 @@ public class Sphere : MonoBehaviour
 {
     public GameObject prefab;
     private bool flag = true;
+    private float pullTime = 0.0f;
     void Start()
     {
         Application.targetFrameRate = 60;
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && flag == true)
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            pullTime += 1.0f;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) && flag ==true && pullTime >= 60)
         {
             StartCoroutine(CreateWave());
             flag = false;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            pullTime = 0.0f;
         }
     }
     IEnumerator CreateWave()
