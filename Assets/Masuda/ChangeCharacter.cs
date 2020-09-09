4df120b7a09a7bd36b56e93e2be3764452275654
ryuga-masuda <m0119260b1@edu.teu.ac.jp>
2020-09-09 22:12:18 +0900

    no message

--- Assets/Masuda/ChangeCharacter.cs
+++ Assets/Masuda/ChangeCharacter.cs
@@ -6,7 +6,11 @@ public class ChangeCharacter : MonoBehaviour
 {
     GameObject FirstChicken;
     GameObject SecondChicken;
+    GameObject ThirdChicken;
+    GameObject FinalChicken;
     public int FirstEvo = 100;
+    public int SecondEvo = 200;
+    public int ThirdEvo = 300;
     public int ScorePoint;
 
     // Start is called before the first frame update
@@ -14,12 +18,16 @@ public class ChangeCharacter : MonoBehaviour
     {
         this.FirstChicken = GameObject.Find("1stChicken");
         this.SecondChicken = GameObject.Find("2ndChicken");
+        this.ThirdChicken = GameObject.Find("3rdChicken");
+        this.FinalChicken = GameObject.Find("FinalChicken");
     }
 
     void Change()
     {
         FirstChicken.SetActive(true);
         SecondChicken.SetActive(false);
+        ThirdChicken.SetActive(false);
+        FinalChicken.SetActive(false);
     }
 
     // Update is called once per frame
@@ -29,6 +37,16 @@ public class ChangeCharacter : MonoBehaviour
         {
             FirstChicken.SetActive(false);
             SecondChicken.SetActive(true);
+        }
+        if (ScorePoint >= SecondEvo)
+        {
+            SecondChicken.SetActive(false);
+            ThirdChicken.SetActive(true);
+        }
+        if (ScorePoint >= ThirdEvo)
+        {
+            ThirdChicken.SetActive(false);
+            FinalChicken.SetActive(true);
         }
     }
 }
