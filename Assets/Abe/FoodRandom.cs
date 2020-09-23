eed2aa1cbf7400610bc072063b708200d19ea7a6
DESKTOP-TFFEFOF\assta <ac.starwars1102@gmail.com>
2020-09-23 17:22:56 +0900

    マップ１ver.2

---
+++ Assets/Abe/FoodRandom.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodRandom : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject Food;
+    
+    float x, y,z;
+    int i = 0;
+
+
+
+    void Start()
+    {
+        i++;
+        {
+            if (i < 5)
+            {
+                x = Random.Range(-140f, 140f);
+                y = Random.Range(2f, 10f);
+                z = Random.Range(-140f, 140f);
+                
+                Instantiate(Food, new Vector3(x, y, z), Quaternion.identity);               
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+
+   
+
+}
